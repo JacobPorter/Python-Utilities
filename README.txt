430c56ec7a89c52c2a3440243be7d71112f14cee
Jacob Porter <jastuper@gmail.com>
2012-04-09 08:09:05 -0700

    updated comments and usage information

--- README.txt
+++ README.txt
@@ -1,12 +1,53 @@
-deduper.py is a pytyhon utility that helps you find duplicate files in a directory and its subdirectories.  Use at your own risk.  There is no guarantee of good functioning.
 
-It will write a text file giving a list of files that are potentially duplicates of each other.  It provides helpful run time messages to std.out
+deduper.py
+--------------------------------
+deduper.py is a pytyhon utility that helps you find duplicate file contents in a directory and its subdirectories.  Use at your own risk.  There is no guarantee of good functioning.
+
+Built using python 2.7.  Python must be installed for this file to be run.
+Doesn't need any non-native modules to run.  Uses hashlib.
+
+It will write a text file giving a list of files that are potentially duplicates of each other.  Some files marked as duplicates may not be duplicates because of hash collisions.
+The process provides helpful run time messages to std.out.
 
 
-EXAMPLE USAGE : python deduper.py --path="C:/folder name/" --sha
+EXAMPLE USAGE : 
+$$>python deduper.py --path="C:/folder name/" --sha
 
-Will use the SHA256 algorithm for finding duplicate files in C:/folder.  Will write a list of potential duplicate files to the C:/folder name/dups.txt
+Will use the SHA256 algorithm for finding duplicate files in C:/folder name/.  Will write a list of potential duplicate files to the C:/folder name/dups.txt by default, but this can be configured.
+Can also use the MD5 hash by using --md5 instead of --sha for the runtime parameter.
+SHA256 is the default when no parameter is specified.
 
-Can also use the MD5 hash by using --md5 instead of --sha
 
-SHA256 is the default
+
+OTHER USAGE:
+In the command line after the file name, the following parameters can be specified.
+ --path="pathname" to specify the super directory that you would like to search.  It automatically searches all subdirectories.  The default is the root directory.
+ --sha to specify using the sha256 hash algorithm.  This is turned on by default
+ --md5 to use the md5 hash function.
+ --read="int_value"  The int_value is the number of bytes to read from each file to determine if the file is a duplicate.  
+A larger value means a lower likelihood of hash collisions and thus fewer false duplicates, but it will take longer to run.  The default is 100,000 bytes = 100 kb.
+ --pickle This paramter periodically (every 1000 files) writes the hash table for the files.  Useful if the system crashes, then you have some information,
+but it does not affect the process.  The default is to turn this off.
+ --pickle_file="pickle path and file name"  Sets the path and file name of the pickle file.  Defaults to "dups_pickle.dmp" and writes to --path
+ --dup_file="dup path and file name"  Sets the location and file name of the duplicate file that is written at the end of the process
+Defaults to "dups.txt" and writes the file to the path in --path
+
+
+KNOWN PROBLEMS AND ISSUES:
+Some files could be marked as duplicates when they are not.  This could be because some files will have many identical bytes at the top of the file but different bytes at the end.
+Deduper.py uses the first bunch of bytes (as well as the file size) to hash the file and compares hashes from that to determine if the file is a duplicate.
+
+This utility ignores the filename and hashes the first bunch of bytes of the file concatenated with the file size.  
+This is okay since duplicate file contents might not have the same file name.
+
+Always recurses down directories.  No way yet to control this yet.
+
+If the duplication hash grows to large for RAM, the process will crash without completing.  This could be fixed by writing to disc using the existing pickle mechanics.
+
+If the number of files to search is to much to hold in an integer field, the process will crash.
+
+If it can't read a file, it skips it.
+
+No graphical interface.
+
+Python must be installed on the system.  Non-executable file.
--- deduper.py
+++ deduper.py
@@ -9,20 +9,37 @@ Copyright 2012 Jacob Porter
 Please attribute me if you use this file.
 '''
 
+#Global variables that control parameters.
+#Number of files to read before printing a "." and pickling
 CNT = 1000
+#Default number of bytes to read for hashing the file
 BYTES_TO_READ = 100000
+#String delimiter for file strings with same hash code
 str_sep = '  !*!!*!  '
+#Default pickle value.  False won't pickle the hash table
 pickle_me = False
+#Should we use the SHA256 hash function?
 USE_SHA256 = True
 
 
 def get_all_possible_dups(path, f_str):
+    '''
+    Starting from the path file, this function
+    searches for all potential duplicate files.
+    It uses a hash function to search for them.
+    f_str is the path of the pickle file to write to.
+    This is in case the process crashes, but it isn't used
+    by the process.
+    '''
     print "Searching for duplicate files under " + path
     total_files = 0
+    # Count the number of files
     for root, dirs, files in os.walk(path):
         for name in files:
             total_files += 1
-    print "Using the first " + str(BYTES_TO_READ) + " bytes in each file to search for duplicate files in " + str(total_files) + " total files."
+    # Print helpful messages
+    print "Using the first " + str(BYTES_TO_READ) + \
+    " bytes in each file to search for duplicate files in " + str(total_files) + " total files."
     if (pickle_me):
         print "Duplication hash will be periodically pickled at: " + f_str 
     else:
@@ -32,26 +49,32 @@ def get_all_possible_dups(path, f_str):
     else:
         str_hash = "MD5"
     print "Using the " + str_hash + " hash function."
+    # Set up hash tables and counting variables
     hashes = {}
     dups = {}
     count = 0
     num_unread = 0
+    # Search through all the files
     for root, dirs, files in os.walk(path):
         for name in files:
             item = os.path.join(root, name)
+            # Update status and pickle hash table if necessary
             if (count == CNT):
                 count = 0
                 sys.stdout.write('.')
                 if (pickle_me):
                     pickle_file(f_str, dups)
             count += 1
+            # Try opening a file.  Skip if can't open
             try:
                 f = open(item, 'rb')
                 s = f.read(BYTES_TO_READ)
             except:
                 num_unread += 1
                 continue
+            # Append the file size to avoid collisions
             s += str(os.path.getsize(item))
+            # Get the hash code for the file
             if (USE_SHA256):
                 #my_raw = Crypto.Hash.SHA256.new(s)
                 my_raw = hashlib.sha256(s)
@@ -60,6 +83,7 @@ def get_all_possible_dups(path, f_str):
                 my_raw = hashlib.md5(s)
             myhash = my_raw.hexdigest()
             f.close()
+            # Search for duplicates and store path if necessary
             if (hashes.has_key(myhash)):
                 if (dups.has_key(myhash)):
                     dups[myhash] += str_sep + item
@@ -67,18 +91,28 @@ def get_all_possible_dups(path, f_str):
                     dups[myhash] = item + str_sep + hashes[myhash] 
             else:
                 hashes[myhash] = item
+    # Write final pickle and return duplicates hash
     if (pickle_me):
         pickle_file(f_str, dups)
     sys.stdout.write('\n')
-    print "A total of " + str(total_files - num_unread) + " files were able to be read.  There were " + str(num_unread) + " unread files."
+    print "A total of " + str(total_files - num_unread) + \
+    " files were able to be read.  There were " + str(num_unread) + " unread files."
     return dups
 
 def pickle_file(mp, stuff):
+    '''
+    Pickle stuff into file mp
+    '''
     f1 = open(mp , 'wb')
     cPickle.dump(stuff, f1, protocol=cPickle.HIGHEST_PROTOCOL)
     f1.close()
 
 def examine_dups(d, path_to_dup_list):
+    '''
+    Search through the duplicate hash in d
+    Write the paths to the potential duplicate files to
+    path_to_dup_list
+    '''
     print "Writing duplicate file list: " + path_to_dup_list
     head_str =  "Number of potential duplicates found: " + str(len(d))
     print head_str
@@ -93,20 +127,31 @@ def examine_dups(d, path_to_dup_list):
     
 
 def main():
+    '''
+    This kicks off the process and 
+    parses the options from the arguments.
+    '''
     global USE_SHA256, pickle_me, BYTES_TO_READ
     p = optparse.OptionParser()
+    # Path of the super directory to search for duplicates
     p.add_option('--path', '-p', default ="/")
+    # Will use sha256 if this is true
     p.add_option('--sha', '-s', action='store_true')
+    # Will use md5 if this is true
     p.add_option('--md5', '-m', action='store_true')
+    # Will pickle the output hash table if true
     p.add_option('--pickle', '-l', action='store_true')
+    # The number of bytes to read for each file to hash
     p.add_option('--read', '-r', default = "100000")
+    # The path to the pickle file
     p.add_option('--pickle_file', '-e', default="dups_pickle.dmp")
+    # The path to the text file to write duplicate file paths
     p.add_option('--dup_file', '-d', default="dups.txt")
     options, arguments = p.parse_args()
-    if options.sha:
-        USE_SHA256 = True
     if options.md5:
         USE_SHA256 = False
+    if options.sha:
+        USE_SHA256 = True
     if options.pickle:
         pickle_me = True
     path = options.path
